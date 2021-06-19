e10d49d2403eda57da6518389367800f99021aaa
siddharthpaul20 <siddharthpaul18@gmail.com>
2021-06-20 03:23:31 +0530

    3rd part code commit: title-message.component, scroll-down.component, my-carousal.component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from './components/my-carousal/my-carousal.component';
+import TitleMessage from './components/title-message/title-message.component';
 
 function App() {
   return (
     <div>
-      <MyNavbar/>
-      MY PORTFOLIO
+      <MyNavbar />
+      <MyCarousal />
+      <TitleMessage />
     </div>
   );
 }
